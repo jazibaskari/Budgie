36b83dd0776bc069f38256d4cc30da6f6c6de136
Jazib Askari <jazibaskari@Jazibs-MacBook-Pro.local>
2026-04-09 13:50:44 +0100

    feat(auth): initialise express server with azure cosmos and google oauth

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -1,14 +1,26 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const configurePassport = () => {
   passport.use(new GoogleStrategy({
-      clientID: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: "/api/auth/google/callback"
-    },
-    (token, tokenSecret, profile, done) => {
-      return done(null, profile);
-    }
-  ));
+    clientID: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  },
+  async (accessToken, refreshToken, profile, done) => {
+
+    // Passing user profile  
+    return done(null, profile);
+  }));
+
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+
+  passport.deserializeUser((obj: any, done) => {
+    done(null, obj);
+  });
 };
